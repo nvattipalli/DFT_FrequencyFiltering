18952a6367c347db36b8ef4c57aeb5cf5314372e
pranav <pmantini@uh.edu>
2017-10-17 22:31:28 -0500

    Initial Commit with DFT Skeleton

---
+++ DFT/DFT.py
@@ -0,0 +1,46 @@
+# For this part of the assignment, please implement your own code for all computations,
+# Do not use inbuilt functions like fft from either numpy, opencv or other libraries
+
+
+class DFT:
+
+    def forward_transform(self, matrix):
+        """Computes the forward Fourier transform of the input matrix
+        takes as input:
+        matrix: a 2d matrix
+        returns a complex matrix representing fourier transform"""
+
+
+
+
+        return matrix
+
+    def inverse_transform(self, matrix):
+        """Computes the inverse Fourier transform of the input matrix
+        matrix: a 2d matrix (DFT) usually complex
+        takes as input:
+        returns a complex matrix representing the inverse fourier transform"""
+
+
+
+        return matrix
+
+
+    def discrete_cosine_tranform(self, matrix):
+        """Computes the discrete cosine transform of the input matrix
+        takes as input:
+        matrix: a 2d matrix
+        returns a matrix representing discrete cosine transform"""
+
+
+
+        return matrix
+
+
+    def magnitude(self, matrix):
+        """Computes the magnitude of the DFT
+        takes as input:
+        matrix: a 2d matrix
+        returns a matrix representing magnitude of the dft"""
+
+        return matrix
---
+++ dip_hw2_dft.py
@@ -0,0 +1,66 @@
+"""dip_hw2_dft.py: Starter file to run howework 2"""
+
+#Example Usage: ./dip_hw1_dft
+#Example Usage: python dip_hw1_dft
+
+
+__author__      = "Pranav Mantini"
+__email__ = "pmantini@uh.edu"
+__version__ = "1.0.0"
+
+import cv2
+import sys
+from DFT import DFT
+from numpy.random import rand
+from datetime import datetime
+
+
+def display_image(window_name, image):
+    """A function to display image"""
+    cv2.namedWindow(window_name)
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
+
+
+def main():
+    """ The main funtion that parses input arguments, calls the approrpiate
+     interpolation method and writes the output image"""
+
+    # intialize a matrix of 25X25 pixels
+    input_matrix = rand(25,25)
+    print("---------------Input Matrix----------------")
+    print(input_matrix)
+
+    dft_obj = DFT.DFT()
+
+    #Compute DFT
+    fft_matrix = dft_obj.forward_transform(input_matrix)
+    print("---------------Forward Fourier Transform----------------")
+    print(fft_matrix)
+
+    #Compute the magnitude of the dft
+    magnitude_matrix = dft_obj.magnitude(fft_matrix)
+    print("---------------Magnitude of the Forward Fourier Transform ----------------")
+    print(magnitude_matrix)
+
+    #Compute the inverse Fourier transfrom
+    ift_matrix = dft_obj.inverse_transform(fft_matrix)
+    print("---------------Inverse Fourier Transform----------------")
+    print(ift_matrix)
+
+    # Compute the discrete cosine transform
+    dct_matrix = dft_obj.discrete_cosine_tranform(input_matrix)
+    print("---------------Discrete Cosine Transform----------------")
+    print(dct_matrix)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
